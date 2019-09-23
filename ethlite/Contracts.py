cd6504f16e80d9dbc13ccf9bf88c8ca184aed0ba
Emiliano A. Billi <emiliano.billi@gmail.com>
2019-09-22 21:54:34 -0300

    Update Contracts.py

--- ethlite/Contracts.py
+++ ethlite/Contracts.py
@@ -133,7 +133,10 @@ class ContractFunction(object):
         data = self.signature
 
       result = self.contract.jsonrpc_provider.eth_call({'to': self.contract.address, 'data': data},'latest')['result']
-      print(result)
+
+      outputs = [o['type'] for o in self.outputs ]
+      return AbiEncoder.decode(outputs,result)
+
     else:
       # raise
       pass
@@ -188,4 +191,5 @@ if __name__ == '__main__':
   abi = json.loads('[{"constant":false,"inputs":[{"name":"u","type":"uint256"},{"name":"i","type":"int256"}],"name":"change","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"getValues","outputs":[{"name":"","type":"uint256"},{"name":"","type":"int256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"val","type":"uint256"}],"name":"change_uint","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"val","type":"int256"}],"name":"change_int","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"anonymous":false,"inputs":[{"indexed":true,"name":"changer","type":"address"},{"indexed":false,"name":"u","type":"uint256"}],"name":"UintChange","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"changer","type":"address"},{"indexed":false,"name":"u","type":"int256"}],"name":"IntChange","type":"event"}]')
   contract = Contract(address,abi)
   contract.jsonrpc_provider = 'https://kovan.infura.io'
-  contract.getValues.call()
+  o = contract.getValues.call()
+  print(o)
