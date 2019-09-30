6f4e7ac3486204e336f23bb2fc62c91cd3832ee8
Emiliano A. Billi <emiliano.billi@gmail.com>
2019-09-30 16:42:47 -0300

    pip

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="ethlite",
+    version="0.0.1",
+    author="Emiliano A. Billi",
+    author_email="emiliano.billi@gmail.com",
+    description="A tiny web3/python alternative to interact with any ethereum compatible blockchain",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/emilianobilli/ethlite",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+)
