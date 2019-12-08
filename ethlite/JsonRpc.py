63b6b82c666bd3eddcbdf902aff49953f667e90e
Emiliano A. Billi <emiliano.billi@gmail.com>
2019-12-07 21:01:42 -0300

    Update JsonRpc.py

--- ethlite/JsonRpc.py
+++ ethlite/JsonRpc.py
@@ -8,14 +8,19 @@ import requests
 class JsonRpcError(Exception):
   pass
 
+
 valid_url_re = compile(
-  r'^(?:http)s?://' # http:// or https://
-  r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|' #domain...
-  r'localhost|' #localhost...
-  r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})' # ...or ip
-  r'(?::\d+)?' # optional port
+  r'^(?:http)s?://'  # http:// or https://
+  r'(?:[^:@]+?:[^:@]*?@|)'  # basic auth
+  r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+'
+  r'(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'  # domain...
+  r'localhost|'  # localhost...
+  r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}|'  # ...or ipv4
+  r'\[?[A-F0-9]*:[A-F0-9:]+\]?)'  # ...or ipv6
+  r'(?::\d+)?'  # optional port
   r'(?:/?|[/?]\S+)$', IGNORECASE
 )
+
 
 class JsonRpc(object):
 
